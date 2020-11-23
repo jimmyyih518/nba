8426bce708a6db5a72f62ab5edab4cfd3a0bb95f
jimmyyih518 <68363688+jimmyyih518@users.noreply.github.com>
2020-11-22 22:40:22 -0700

    Update Scraper_byMonth.py
    Added fixes for keyerror handling

--- Scraper_byMonth.py
+++ Scraper_byMonth.py
@@ -5,13 +5,22 @@ Created on Sun Nov 22 13:53:13 2020
 @author: JZ2018
 """
 
+#Input Year of season to be scraped:
+year = 2016
+#Input Directory for output csv files:
+output_filedir = 'D:/JZR/nba/boxscores/'
+
+##-----------------------------------------------------------------------------------
+#RUN EVERYTHING BELOW
+
 import os
 #print(os.getcwd())
 from bs4 import BeautifulSoup 
 import requests
 import pandas as pd
 
-
+def isNaN(num):
+    return num != num
 
 team_dictionary = {
     'Atlanta Hawks': 'Atl', 'Boston Celtics': 'Bos', 
@@ -37,7 +46,6 @@ month_dictionary = {'Jan': '01', 'Feb': '02',  'Mar': '03',
 
 month_list = ['september','october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june']
 
-year = 2017
 start_url = "https://www.basketball-reference.com/leagues/NBA_{}_games.html".format(year)
 
 base_url = 'https://www.basketball-reference.com'   #used for formatting each link correctly
@@ -56,9 +64,11 @@ for i in months:
         month_link_array.append(i)  #appending the url for each page to scrape
 #iterating through each month url to scrape the data
 
+df_columns = ['Date', 'Name', 'Team', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA',
+                   '3P%','FT', 'FTA', 'FT%', 'ORB', 
+                   'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '+-','Team_Name' ]
 
-
-def getMonthdata(month, page):
+def getMonthdata(month, page, df_columns):
     page_tocheck_dict = {'Month': [], 'Url': [], 'Index': []}
     box_link_array = []
     all_dates = []
@@ -92,17 +102,20 @@ def getMonthdata(month, page):
     box_link_array.append(page_link_array)
     all_dates.append(page_date_array)
 
-    df_columns = ['Date', 'Name', 'Team', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA',
-                   '3P%','FT', 'FTA', 'FT%', 'ORB', 
-                   'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '+-' ]
+# =============================================================================
+#     df_columns = ['Date', 'Name', 'Team', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA',
+#                    '3P%','FT', 'FTA', 'FT%', 'ORB', 
+#                    'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '+-','Team_Name' ]
+# =============================================================================
     stat_df = pd.DataFrame(columns = df_columns)
+    error_df = pd.DataFrame(columns = df_columns)
     for l, d in zip(box_link_array, all_dates):
         #l = box_link_array[0]
         #d = all_dates[0]
         for link, date in zip(l, d):
             #link = l[0]
             #date = d[0]
-            print(f'{link}\n{date}')
+            #print(f'{link}\n{date}')
             print(f'Currently Scraping {link}')
             response = requests.get(link)
             soup = BeautifulSoup(response.text, 'html.parser')
@@ -129,11 +142,15 @@ def getMonthdata(month, page):
                         if len(player) < 21:
                             continue
                         else:
-                            player = [0 if i == '' else i for i in player]
+                            #player = [0 if i == '' or isNaN(i) else i for i in player]
                             colon = player[0].find(':')
                             time = f'{player[0][:colon]}.{player[0][colon + 1::]}'
                             #print(player, len(player))
-                            player_dic = {'Date': date, 'Name': player[-1], 'Team': team_dictionary[team], 
+                            if team in team_dictionary:
+                                return_team = team_dictionary[team]
+                            else:
+                                return_team = 'NA'
+                            player_dic = {'Date': date, 'Name': player[-1], 'Team': return_team, 
                                           'MP': time,'FG': player[1], 'FGA': player[2], 
                                           'FG%': player[3], '3P': player[4], '3PA': player[5],
                                           '3P%': player[6], 'FT': player[7], 'FTA': player[8], 
@@ -141,12 +158,14 @@ def getMonthdata(month, page):
                                           'DRB': player[11], 'TRB': player[12], 
                                           'AST': player[13], 'STL': player[14], 
                                           'BLK': player[15], 'TOV': player[16], 'PF': player[17],
-                                          'PTS': player[18], '+-': player[19]}
+                                          'PTS': player[18], '+-': player[19], 'Team_Name': team}
                             stat_df = stat_df.append(player_dic, ignore_index=True)
                             continue
                     except KeyError:
                         print(str(date) + str(player[-1]) + ':' + 'Key Error')
-                        #print('Key Error')
+                        print(len(player))
+                        print(player)
+                        #print('Key Error')                      
                         continue
                 # second table
                 table2 = soup.findAll('table', {'class': "sortable stats_table"})
@@ -170,11 +189,15 @@ def getMonthdata(month, page):
                             #print(player)
                             continue
                         else:
-                            player = [0 if i == '' else i for i in player]
+                            #player = [0 if i == '' or isNaN(i) else i for i in player]
                             colon = player[0].find(':')
                             time = f'{player[0][:colon]}.{player[0][colon + 1::]}'
+                            if team in team_dictionary:
+                                return_team = team_dictionary[team]
+                            else:
+                                return_team = 'NA'                            
                             #print(player, len(player))
-                            player_dic = {'Date': date, 'Name': player[-1], 'Team': team_dictionary[team], 
+                            player_dic = {'Date': date, 'Name': player[-1], 'Team': return_team, 
                                           'MP': time,'FG': player[1],'FGA': player[2], 
                                           'FG%': player[3], '3P': player[4], 
                                           '3PA': player[5],'3P%': player[6], 'FT': player[7], 
@@ -183,19 +206,30 @@ def getMonthdata(month, page):
                                           'TRB': player[12], 'AST': player[13],
                                           'STL': player[14], 'BLK': player[15], 
                                           'TOV': player[16], 'PF': player[17],
-                                          'PTS': player[18], '+-': player[19]}
+                                          'PTS': player[18], '+-': player[19], 'Team_Name': team}
                             stat_df = stat_df.append(player_dic, ignore_index=True)
                             continue
                     except KeyError:
                         print(str(date) + str(player[-1]) + ':' + 'Key Error')
+                        print(len(player))
+                        print(player)                      
                         #print('Key Error')
                         continue
     return stat_df
 
 
-month = month_link_array[5][0]
-page = month_link_array[5][1]  
+#month = month_link_array[0][0]
+#page = month_link_array[0][1]  
 
-statdf = getMonthdata(month, page)
-outfile = str(season) + str(month)
-statdf.to_csv(f'D:/JZR/nba/{outfile}.csv')
+
+output_df = pd.DataFrame(columns = df_columns)
+
+for month, page in month_link_array:
+    statdf = getMonthdata(month, page, df_columns)
+    outfile = output_filedir + 'Season_' + str(season) + str(month) + '_BoxScores'
+    statdf.to_csv(f'{outfile}.csv')
+    #output_df.to_csv(f'D:/JZR/nba/boxscores/{outfile}.csv')
+    #output_df = output_df.append(statdf)
+
+
+
